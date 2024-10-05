c8b17c3e26c6d3f37226df7694e43958d76d1f97
Prasad <prasaddrane@gmail.com>
2024-10-05 13:31:52 +0100

    Adding Response Entity ?Handling

--- src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -1,7 +1,10 @@
 package com.ecommerce.project.service;
 
 import com.ecommerce.project.model.Category;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,10 +30,8 @@ public class CategoryServiceImpl implements CategoryService {
     public String deleteCategoryById(Long categoryId) {
         Category category = categoryList.stream().
                 filter(c -> c.getCategoryId().equals(categoryId)).
-                findFirst().orElse(null);
-        if(category == null){
-            return "Category not found :";
-        }
+                findFirst().
+                orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource Not Found"));
         categoryList.remove(category);
         return "Category with categoryId: " + categoryId + " deleted successfully";
     }
