2f013793ce6c579582ee4f71c9b43147be7be88c
Vankyrane <78443716+Vankyrane@users.noreply.github.com>
2024-10-13 00:16:12 +0100

    Adding Resource Not Found Exception (#13)

--- src/main/java/com/ecommerce/project/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/ecommerce/project/exceptions/GlobalExceptionHandler.java
@@ -23,4 +23,10 @@ public class GlobalExceptionHandler {
         });
         return new ResponseEntity<Map<String, String>>(response, HttpStatus.BAD_REQUEST);
     }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> myResourceNotFoundException(ResourceNotFoundException exception){
+        String message = exception.getMessage();
+        return new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
+    }
 }
